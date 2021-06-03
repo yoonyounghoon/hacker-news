ba1c0aa736939ecc6de5a47a6d1cc4e61349435f
yoonyounghoon <yunyounghoon23@gmail.com>
2021-06-03 17:46:56 +0900

    template 방식 적용,  tailwind 설정 추가

--- hackerNews-js/app.js
+++ hackerNews-js/app.js
@@ -16,8 +16,18 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-
-  newsList.push('<ul>');
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
   for(let i= (store.currentPage - 1) * 10; i< store.currentPage * 10; i++){
     newsList.push(`
@@ -26,18 +36,14 @@ function newsFeed() {
         ${newsFeed[i].title} 
       </a>
     </li>
-    `)
+    `);
   }
 
-  newsList.push('</ul>');
-  newsList.push(`
-    <div>
-      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전으로</a>
-      <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음으로</a>
-    </div>
-  `)
+  template = template.replace('{{__news_feed__}}', newsList.join(""));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1 );
+  template = template.replace('{{__next_page__}}', store.currentPage + 1);
 
-  container.innerHTML = newsList.join("");
+  container.innerHTML = template;
 }
 
 function newsDetail() {
