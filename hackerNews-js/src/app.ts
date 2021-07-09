e486a82ed01d605dd3dc3090f5dafc3984f32a6f
yoonyounghoon <yunyounghoon23@gmail.com>
2021-07-09 12:06:57 +0900

    파일분리

---
+++ hackerNews-js/src/app.ts
@@ -0,0 +1,28 @@
+import Router from "./core/router";
+import { NewsDetailView, NewsFeedView }from './page';
+import { Store } from "./types";
+
+const store: Store = {
+  currentPage: 1,
+  feeds: [],
+};
+
+declare global {
+  interface Window { 
+    store: Store;
+  }
+}
+
+window.store = store;
+
+
+const router: Router = new Router();
+const newsFeedView = new NewsFeedView('root');
+const newsDetailView = new NewsDetailView('root');
+
+router.setDefaultPage(newsFeedView);
+
+router.addRoutePath('/page/', newsFeedView);
+router.addRoutePath('/show/', newsDetailView);
+
+router.route();
